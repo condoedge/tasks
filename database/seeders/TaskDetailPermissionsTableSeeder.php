107e135d5493eaf7979d0c569cfa59788ef23332
Ezequiel Imaz <tavelliezequiel@gmail.com>
2024-02-28 17:43:27 -0300

    Adding descriptions to permissions (cannot be null)

--- database/seeders/TaskDetailPermissionsTableSeeder.php
+++ database/seeders/TaskDetailPermissionsTableSeeder.php
@@ -14,26 +14,31 @@ class TaskDetailPermissionsTableSeeder extends Seeder
         $this->createPermission(
             'taskDetails:create',
             'Create task details',
+            'Create task details',
         );
 
         $this->createPermission(
             'taskDetails:updateOfTeam',
             'Update only task details of your team',
+            'Update task details of your team',
         );
 
         $this->createPermission(
             'taskDetails:deleteOfTeam',
             'Delete only task details of your team',
+            'Delete task details of your team',
         );
 
         $this->createPermission(
             'taskDetails:delete',
             'Delete task details',
+            'Delete task details all task details',
         );
 
         $this->createPermission(
             'taskDetails:update',
             'Update task details',
+            'Update task details all task details',
         );
     }
 }
